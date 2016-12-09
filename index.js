8317bfc93478fb9f167733429a221319fb04eb53
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-'use strict';
+'use strict'
 
-var hogan = require('hogan.js');
-var fs  = require('fs');
+var hogan = require('hogan.js')
 
-exports.name = 'hogan';
-exports.outputFormat = 'html';
+exports.name = 'hogan'
+exports.outputFormat = 'html'
 
 exports.compile = function (str, options) {
-  options = options || {};
-  var obj = hogan.compile(str, options);
-  return obj.render.bind(obj);
-};
+  options = options || {}
+  var obj = hogan.compile(str, options)
+  return obj.render.bind(obj)
+}
